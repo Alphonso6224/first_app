8438fbd3f75959625ae669e5865c8a3473e5d046
Alphonso6224 <donaldalphonso11@gmail.com>
2024-01-06 10:55:32 +0100

    Creation de composant et importation dans le app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [HomeComponent],
+  template: `
+    <h1>Default</h1>
+  `,
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'first-app';
+  title = 'default';
 }
