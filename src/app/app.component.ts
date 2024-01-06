005d6b9655a1dd67a5abf18a9de27dc24c5668ba
Alphonso6224 <donaldalphonso11@gmail.com>
2024-01-06 12:39:31 +0100

    Activiation du routage et definition de nouveau itinéraire

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import {Component} from '@angular/core';
 import { HomeComponent } from './home/home.component';
+import {RouterModule} from '@angular/router'
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [HomeComponent],
+  imports: [HomeComponent, RouterModule],
   template: `
     <h1>Default</h1>
   `,
