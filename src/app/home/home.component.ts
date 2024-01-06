e7d4983536f20959bea42718ca7dc37e85892045
Alphonso6224 <donaldalphonso11@gmail.com>
2024-01-06 13:29:07 +0100

    Ajout de la fonction de recherche

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -15,8 +15,21 @@ export class HomeComponent {
   title = "homes";
   housingLocationList: Housinglocation[] = [];
   housingService: HousingService = inject(HousingService);
+  filteredLocationList = this.housingLocationList;
 
   constructor() {
     this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.filteredLocationList = this.housingLocationList;
   }
+
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+    }
+
+    this.filteredLocationList = this.housingLocationList.filter((housingLocation) =>
+    housingLocation?.city.toLowerCase().includes(text.toLowerCase()),)
+  }
+
+
 }
